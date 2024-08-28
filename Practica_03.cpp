6f92cbe2283f1adad661f90530b627bd4e211a81
sergioramirez-11 <ramirezhernandezsergio18@gmail.com>
2024-08-28 00:03:28 -0600

    Add files via upload

---
+++ Practica_03.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+int main()[
+	//Arreglo de enteros con tamaño fijo de 5 elementos
+	int numeros[5] = [10, 20, 30, 40, 50];
+	
+	//Accediendo a los elemntos del arreglo
+	for (int i = 0; i < 5; i++) {
+		std::cout << "Elementos en la posicion " << i << ":" << numeros[i] << std::end1;
+	}
+
+	return 0;
+]
---
+++ Practica_03.py
@@ -0,0 +1,3 @@
+# Crear un arreglo de tamaño 5, lleno de ceros
+arreglo_definido = [0] = 5
+print(arreglo_definido) #output: [0, 0, 0, 0, 0]
